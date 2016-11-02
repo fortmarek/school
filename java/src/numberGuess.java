f2abb2e014733745fc9065ca7a536d02e1eb88b1
fortmarek <marekfort@me.com>
2016-11-02 17:33:28 +0100

    youre lying

--- java/src/numberGuess.java
+++ java/src/numberGuess.java
@@ -21,11 +21,20 @@ public class numberGuess {
 
         while (!are_numbers_same) {
 
-            int guessed_number = guess();
+            try {
+                int guessed_number = guess();
 
-            String answer = get_user_answer();
 
-            are_numbers_same = adjust_min_max(answer, guessed_number);
+                String answer = get_user_answer();
+
+                are_numbers_same = adjust_min_max(answer, guessed_number);
+            }
+            catch(java.lang.IllegalArgumentException e){
+                out.println("YOURE LYIN");
+                are_numbers_same = true;
+            }
+
+
 
         }
 
@@ -53,11 +62,18 @@ public class numberGuess {
 
         switch (answer) {
             case "m":
-                if (max_number < guess - 1)
+                if (max_number == guess - 1){
+                    out.println("YOURE LYIN");
+                    are_numbers_same = true;
+                }
                 max_number = guess - 1;
                 break;
             case "v":
                 min_number = guess + 1;
+                if (max_number == guess + 1){
+                    out.println("YOURE LYIN");
+                    are_numbers_same = true;
+                }
                 break;
             case "t":
                 out.println("COMPUTERS WILL KILL YOU");
