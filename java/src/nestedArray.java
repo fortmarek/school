659b9129799ebe0da228f42bd8208d48155e865b
fortmarek <marekfort@me.com>
2016-11-30 15:59:33 +0100

    modified field

--- java/src/nestedArray.java
+++ java/src/nestedArray.java
@@ -8,38 +8,58 @@ import static java.lang.System.out;
 public class nestedArray {
     public static void main(String[] args) {
         int[][] nested = new int[][] {{1,2},{3,4},{5,6}};
-
-        getTable(2, nested);
+        getTable(2, nested, true);
+        getTable(2, nested, false);
     }
 
-    private static List<String> getTable(int height, int[][] nested) {
+    private static List<String> getTable(int height, int[][] nested, boolean downwards) {
         List<String> rows = new ArrayList<String>();
         rows.add("");
 
-        int rowIndex = 0;
+        int index = 0;
 
         for (int[] nest : nested) {
             for (int number : nest) {
-                String row = rows.get(rowIndex);
+
+
+
+                String row = rows.get(index);
+
                 if (row.length() == 0) {
                     row += String.format("%s", number);
                 }
+
                 else {
                     row += ", " + number;
                 }
 
-                rows.set(rowIndex, row);
+                rows.set(index, row);
 
-                if (rowIndex == 0 && rows.size() < height) {
+                if (index == 0 && rows.size() < height) {
                     rows.add("");
                 }
 
-                if (rowIndex == height - 1) {
-                    rowIndex = 0;
-                    continue;
+
+                if (row.length() == height * 3 - 2 && !downwards) {
+                    rows.add("");
+                    index++;
                 }
 
-                rowIndex++;
+
+
+                if (index == height - 1) {
+
+                    if (downwards) {
+                        index = 0;
+                        continue;
+                    }
+
+                }
+
+                if (downwards) {
+                    index++;
+                }
+
             }
         }
 
