41a95eb3ba31cb456b230038392990c7b46d1367
fortmarek <marekfort@me.com>
2017-05-03 22:02:13 +0200

    createTriangle

--- java/src/errors/Errors.java
+++ java/src/errors/Errors.java
@@ -7,10 +7,17 @@ import java.util.*;
 
 public class Errors {
     public static void main(String args[]) {
-        runError();
+        createTriangle();
     }
 
-    private static void runError() {
+    private static void createTriangle() {
+
+        Scanner sc = new Scanner(System.in);
+        int a = getSide("a");
+        int b = getSide("b");
+        int c = getSide("c");
+
+        /*
         String hmotnost;
         Scanner sc = new Scanner(System.in);
         System.out.println("Zadej hmotnost: ");
@@ -30,5 +37,19 @@ public class Errors {
         catch (NullPointerException e) {
 
         }
+        */
+    }
+
+    private static int getSide(String sideName) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println(String.format("Zadejte stranu %s: ", sideName));
+            return sc.nextInt();
+        }
+        catch(InputMismatchException e) {
+            System.err.println("Pouze čísla");
+            getSide(sideName);
+        }
+        return 0;
     }
 }
